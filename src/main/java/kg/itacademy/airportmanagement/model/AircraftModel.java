6ad86e25db5dafc3abf126d3adad30ebb534d6e0
Koshoi <koshoy.ibraev@gmail.com>
2022-04-26 20:46:07 +0600

    init

---
+++ src/main/java/kg/itacademy/airportmanagement/model/AircraftModel.java
@@ -0,0 +1,11 @@
+package kg.itacademy.airportmanagement.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AircraftModel {
+    private Long id;
+    private String aircraftName;
+}
---
+++ src/main/java/kg/itacademy/airportmanagement/service/AircraftService.java
@@ -0,0 +1,17 @@
+package kg.itacademy.airportmanagement.service;
+
+import kg.itacademy.airportmanagement.model.AircraftModel;
+
+import java.util.List;
+
+//public interface AircraftService {
+//    AircraftModel create(AircraftModel aircraftModel);
+//
+//    boolean update(AircraftModel aircraftModel);
+//
+//    boolean deleteById(Long id);
+//
+//    AircraftModel getById(Long id);
+//
+//    List<AircraftModel> getAllByAirportName(String aircraftName);
+//}
